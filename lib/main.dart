878f064bba5b1a7b8e00130f798bea275c6ac0a0
Probe <110994698+xProbe@users.noreply.github.com>
2026-05-20 11:39:21 +0700

    Revert "fix: perbaikan error"
    This reverts commit 65af19f711cc37e521a1bc1339dc57ae99121ee8.

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,15 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get/get.dart';
 import 'models/cart_item.dart';
-import 'services/notification_service.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  // Inisialisasi Hive untuk penyimpanan lokal keranjang
   await Hive.initFlutter();
   Hive.registerAdapter(CartItemAdapter());
   await Hive.openBox<CartItem>('cartBox');
-  
-  // Inisialisasi Notifikasi Lokal (Bonus Nilai)
-  try {
-    await NotificationService.init();
-  } catch (e) {
-    print("Gagal menginisialisasi NotificationService: $e");
-  }
   
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String? username = prefs.getString('username');
